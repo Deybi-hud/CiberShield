68ca3243bf0998b22675e9e48aa76b5f05801866
Deybi-hud <hu4nt4r@gmail.com>
2025-09-05 16:56:42 -0400

    Agregar configuración de lanzamiento para CiberShieldApplication y actualizar ajustes de Java en VSCode

--- app/app/src/main/java/com/cibershield/app/repository/ProductoRepositorio.java
+++
@@ -1,8 +0,0 @@
-package com.cibershield.app.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cibershield.app.model.Producto;
-
-public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
-
-}
