ed9c317c209e72446da45dbbf52b4ab77c0ee069
Deybi-hud <hu4nt4r@gmail.com>
2025-09-05 00:26:02 -0400

    Add initial HTML structure and JavaScript files for CiberShield application
    - Created carrito.html with layout for shopping cart, including product details and total calculation.
    - Created index.html for product listing with buttons to add items to the cart.
    - Added empty carrito.js and main.js files for future JavaScript functionality.
    - Implemented basic test class CiberShieldApplicationTests for application context loading.

---
+++ app/app/src/main/java/com/cibershield/app/CiberShieldApplication.java
@@ -0,0 +1,13 @@
+package com.cibershield.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiberShieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiberShieldApplication.class, args);
+	}
+
+}
---
+++ app/app/src/test/java/com/cibershield/app/CiberShieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cibershield.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiberShieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
